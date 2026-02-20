08b7cd5f9f77843faf1a595191e17cfd088b17c3
zuziacz555-hue <zuziacz555@gmail.com>
2026-02-20 22:09:55 +0100

    Cleanup temporary scripts to fix build

--- check-unionki.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from './src/lib/generated/client2'
-const prisma = new PrismaClient()
-async function main() {
-    const users = await prisma.user.findMany()
-    console.log('USERS IN DB:', JSON.stringify(users, null, 2))
-}
-main().finally(() => prisma.$disconnect())
